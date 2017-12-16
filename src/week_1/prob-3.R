89e6a56c8baf7c266377f8df1548ecbbdc7071b6
Tran Hau <hauvipapro@gmail.com>
2017-12-16 16:08:35 +0700

    bai 3

--- src/week_1/prob-3.R
+++ src/week_1/prob-3.R
@@ -1,4 +1,16 @@
-prob_2_3<-function()
-{
-    data<-read.csv("data01.csv",header=TRUE)
+prob_2_3 <- function() {
+    data <- read.csv("~/proj/R/data/data01.csv", header = TRUE)
+    index <- c()
+    for (i in 1:nrow(data)) {
+        if (data$Age[i] <= 60) {
+            index <- c(index, 0)
+        } else if (data$Age[i] <= 70) {
+            index <- c(index, 1)
+        } else if (data$Age[i] <= 80) {
+            index <- c(index, 2)
+        } else {
+            index <- c(index, 3)
+        }
+    }
+    cbind(data, index)
 }
