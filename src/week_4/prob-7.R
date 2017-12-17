c19b9f0daa06bfa4877110d19c1ee1cf0bd04b5a
Tran Hau <hauvipapro@gmail.com>
2017-12-17 21:25:21 +0700

    fix bai 7

--- src/week_4/prob-7.R
+++ src/week_4/prob-7.R
@@ -1,6 +1,5 @@
 func_4_7 <- function() {
-    par(mfrow = c(2, 1))
     plot(0:50, dbinom(0:50, 50, 0.4), type = "h", col = "red")
     # Xap xi nhi thuc bang phan phoi chuan E = np = 20 Var = npq = 12
-    plot(0:50, dnorm(0:50, 20, 12), type = "h", col = "blue")
+    curve(dnorm(x, 20, sqrt(12)), 0, 50, add = T)
 }
