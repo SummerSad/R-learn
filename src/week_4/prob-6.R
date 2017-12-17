e1fd1b4f5bdbd88b62b4b3e2b3deab1b511ee070
Tran Hau <hauvipapro@gmail.com>
2017-12-17 20:56:12 +0700

    bai 6

--- src/week_4/prob-6.R
+++ src/week_4/prob-6.R
@@ -1,9 +1,8 @@
-func_4_6<-function()
-{
-    # ylim la khoang cach ngan nhat cho truc y
-    # nhi thuc
-    plot(0:50,dbinom(0:50,50,0.08),ylim=c(0,0.25),type="h", main="X ~ B(50,0.08)")
-    x11()
+func_4_6 <- function() {
+    # chia thanh 2 cot 1 dong
+    par(mfrow = c(2, 1))
+    # ylim la khoang cach ngan nhat cho truc y nhi thuc
+    plot(0:50, dbinom(0:50, 50, 0.08), ylim = c(0, 0.25), type = "h", main = "X ~ B(50,0.08)")
     # xap xi bang pois
-    plot(0:50,dpois(0:50,4),ylim=c(0,0.25),type="h", main="X ~ P(4)")
+    plot(0:50, dpois(0:50, 4), ylim = c(0, 0.25), type = "h", main = "X ~ P(4)")
 }
